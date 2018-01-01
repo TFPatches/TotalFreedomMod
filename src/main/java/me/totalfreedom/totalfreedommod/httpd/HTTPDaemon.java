43ef1fb108611b7597b29f3025beb65380a4a812
ZeroEpoch1969 <13510767+ZeroEpoch1969@users.noreply.github.com>
2018-01-01 16:31:28 -0700

    Bug fixes and improvements

--- src/main/java/me/totalfreedom/totalfreedommod/httpd/HTTPDaemon.java
+++ src/main/java/me/totalfreedom/totalfreedommod/httpd/HTTPDaemon.java
@@ -13,6 +13,8 @@ import me.totalfreedom.totalfreedommod.config.ConfigEntry;
 import me.totalfreedom.totalfreedommod.httpd.NanoHTTPD.HTTPSession;
 import me.totalfreedom.totalfreedommod.httpd.NanoHTTPD.Response;
 import me.totalfreedom.totalfreedommod.httpd.module.HTTPDModule;
+import me.totalfreedom.totalfreedommod.httpd.module.Module_admins;
+import me.totalfreedom.totalfreedommod.httpd.module.Module_bans;
 import me.totalfreedom.totalfreedommod.httpd.module.Module_dump;
 import me.totalfreedom.totalfreedommod.httpd.module.Module_file;
 import me.totalfreedom.totalfreedommod.httpd.module.Module_help;
@@ -24,6 +26,7 @@ import me.totalfreedom.totalfreedommod.httpd.module.Module_schematic;
 import me.totalfreedom.totalfreedommod.util.FLog;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.io.FilenameUtils;
 
 public class HTTPDaemon extends FreedomService
 {
@@ -53,6 +56,8 @@ public class HTTPDaemon extends FreedomService
 
         // Modules
         modules.clear();
+        module("admins", Module_admins.class, true);
+        module("bans", Module_bans.class, true);
         module("dump", Module_dump.class, true);
         module("file", Module_file.class, true);
         module("help", Module_help.class, false);
@@ -159,6 +164,12 @@ public class HTTPDaemon extends FreedomService
                 if (mimetype == null || mimetype.trim().isEmpty())
                 {
                     mimetype = MIME_DEFAULT_BINARY;
+                }
+                
+                // Some browsers like firefox download the file for text/yaml mime types
+                if (FilenameUtils.getExtension(file.getName()).equals("yml"))
+                {
+                    mimetype = NanoHTTPD.MIME_PLAINTEXT;
                 }
 
                 response = new Response(Response.Status.OK, mimetype, new FileInputStream(file));
