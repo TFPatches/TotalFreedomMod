eda7216b4cad88dc21378dff895629d1981d93ec
Telesphoreo <craftyswans@gmail.com>
2018-06-08 01:02:05 -0700

    Got it

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_links.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_links.java
@@ -13,17 +13,22 @@ import java.util.Map;
 
 @CommandPermissions(level = Rank.OP, source = SourceType.BOTH)
 @CommandParameters(description = "Get social media links.", usage = "/<command>", aliases = "link")
-public class Command_links extends FreedomCommand {
+public class Command_links extends FreedomCommand
+{
     @Override
-    protected boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
+    protected boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
         ConfigurationSection section = plugin.getConfig().getConfigurationSection("social_links");
-        if (section != null) {
+        if (section != null)
+        {
             Map<String, Object> values = section.getValues(false);
 
             List<String> lines = new ArrayList<>();
 
-            for (String key : values.keySet()) {
-                if (!(values.get(key) instanceof String)) {
+            for (String key : values.keySet())
+            {
+                if (!(values.get(key) instanceof String))
+                {
                     continue;
                 }
                 String link = (String) values.get(key);
@@ -33,7 +38,9 @@ public class Command_links extends FreedomCommand {
             sender.sendMessage(ChatColor.AQUA + "Social Media Links:");
             sender.sendMessage(lines.toArray(new String[0]));
             return true;
-        } else {
+        }
+        else
+        {
             sender.sendMessage(ChatColor.RED + "There are no links added in the configuration file.");
         }
         return true;
