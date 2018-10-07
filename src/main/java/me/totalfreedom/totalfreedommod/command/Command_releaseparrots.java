cc401407667ab576cb8a2b1aecda35ecfe53fba6
Model 3 <craftyswans@gmail.com>
2018-10-06 19:40:31 -0700

    forgot these

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_releaseparrots.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_releaseparrots.java
@@ -11,12 +11,18 @@ import org.bukkit.entity.Player;
 @CommandParameters(description = "Release parrots from your shoulders.", usage = "/<command>", aliases = "removeparrots")
 public class Command_releaseparrots extends FreedomCommand
 {
-
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
         Entity leftShoulderEntity = playerSender.getShoulderEntityLeft();
         Entity rightShoulderEntity = playerSender.getShoulderEntityRight();
+
+        if (rightShoulderEntity == null && leftShoulderEntity == null)
+        {
+            msg("No parrots were detected on either of your shoulders.");
+            return true;
+        }
+
         if (leftShoulderEntity != null && leftShoulderEntity.getType().equals(EntityType.PARROT))
         {
             playerSender.setShoulderEntityLeft(null);
