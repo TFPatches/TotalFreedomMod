687cc59ae5673a6293b93b135d74cd22257d9ca9
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2020-01-12 00:29:01 -0500

    add useful stuff to lockup

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_lockup.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_lockup.java
@@ -1,19 +1,24 @@
 package me.totalfreedom.totalfreedommod.command;
 
+import me.totalfreedom.totalfreedommod.freeze.FreezeData;
 import me.totalfreedom.totalfreedommod.player.FPlayer;
-import me.totalfreedom.totalfreedommod.rank.Rank;
 import me.totalfreedom.totalfreedommod.util.FUtil;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import me.totalfreedom.totalfreedommod.rank.Rank;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.command.CommandSender;
 import org.bukkit.scheduler.BukkitTask;
-
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 @CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.BOTH, blockHostConsole = true)
 @CommandParameters(description = "Block a player's Minecraft input. This is evil, and I never should have wrote it.", usage = "/<command> <all | purge | <<partialname> on | off>>")
 public class Command_lockup extends FreedomCommand
 {
-
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
@@ -38,6 +43,19 @@ public class Command_lockup extends FreedomCommand
                 }
 
                 msg("Unlocked all players.");
+            }
+            else if (args[0].equalsIgnoreCase("noob"))
+            {
+                FUtil.adminAction(sender.getName(), "Locking up all non-admins", true);
+                for (Player player : server.getOnlinePlayers())
+                {
+                    if (!plugin.al.isAdmin(player))
+                    {
+                        startLockup(player);
+                    }
+                    msg("Locked up all non-admins.");
+                }
+                msg("Locked up all non-admins.");
             }
             else
             {
@@ -93,30 +111,46 @@ public class Command_lockup extends FreedomCommand
         {
             lockupScheduleId.cancel();
             playerdata.setLockedUp(false);
+            playerdata.setInvSee(false);
             playerdata.setLockupScheduleId(null);
         }
     }
 
+    private ItemStack makeItem(Material mat, String name)
+    {
+        ItemStack lockitem = new ItemStack(mat);
+        ItemMeta meta = lockitem.getItemMeta();
+        meta.setDisplayName(name);
+        lockitem.setItemMeta(meta);
+        return lockitem;
+    }
     private void cancelLockup(final Player player)
     {
         cancelLockup(plugin.pl.getPlayer(player));
+        final FreezeData fd = plugin.pl.getPlayer(player).getFreezeData();
+        fd.setFrozen(false);
+        player.closeInventory();
     }
 
     private void startLockup(final Player player)
     {
         final FPlayer playerdata = plugin.pl.getPlayer(player);
-
         cancelLockup(playerdata);
-
+        final Inventory lockinv = server.createInventory(null, 9, ChatColor.GOLD + "Lockup");
         playerdata.setLockedUp(true);
+        final FreezeData fd = plugin.pl.getPlayer(player).getFreezeData();
+        fd.setFrozen(true);
         playerdata.setLockupScheduleId(new BukkitRunnable()
         {
             @Override
             public void run()
             {
                 if (player.isOnline())
-                {
-                    player.openInventory(player.getInventory());
+                {   
+                    playerdata.setInvSee(true);
+                    lockinv.clear();
+                    lockinv.setItem(4, makeItem(Material.REDSTONE_BLOCK, ChatColor.RED + sender.getName() + ChatColor.GOLD + " placed you in lockup."));
+                    player.openInventory(lockinv);
                 }
                 else
                 {
@@ -124,5 +158,7 @@ public class Command_lockup extends FreedomCommand
                 }
             }
         }.runTaskTimer(plugin, 0L, 5L));
+
     }
+    
 }
