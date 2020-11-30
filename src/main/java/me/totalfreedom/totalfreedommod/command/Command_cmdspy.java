716bc293f83ed55fb7e736b9924bf42b752822c2
DragonSlayer2189 <59424467+DragonSlayer2189@users.noreply.github.com>
2020-11-29 22:27:20 -0500

    Making TFM comply with itself

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_cmdspy.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_cmdspy.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.ADMIN, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Spy on commands", usage = "/<command>", aliases = "commandspy")
+@CommandParameters(description = "Spy on commands.", usage = "/<command>", aliases = "commandspy")
 public class Command_cmdspy extends FreedomCommand
 {
 
