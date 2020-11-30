62302b2649307a3d772b2919f41f32f71620d1fd
DragonSlayer2189 <59424467+DragonSlayer2189@users.noreply.github.com>
2020-11-29 23:09:20 -0500

    Making TFM Comply With Itself

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_linkdiscord.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_linkdiscord.java
@@ -9,7 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.OP, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Link your Discord account to your Minecraft account", usage = "/<command> [<name> <id>]")
+@CommandParameters(description = "Link your Discord account to your Minecraft account.", usage = "/<command> <name> <id>")
 public class Command_linkdiscord extends FreedomCommand
 {
 
@@ -55,7 +55,7 @@ public class Command_linkdiscord extends FreedomCommand
             Discord.LINK_CODES.put(code, data);
         }
         msg("Your linking code is " + ChatColor.AQUA + code, ChatColor.GREEN);
-        msg("Take this code and DM the server bot (" + plugin.dc.formatBotTag() + ") the code (do not put anything else in the message, only the code)");
+        msg("Take this code and DM the server bot (" + plugin.dc.formatBotTag() + ") the code (do not put anything else in the message, only the code.)");
         return true;
     }
 }
