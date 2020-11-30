1be01230f0c466278544decf51adde0c46e0175a
DragonSlayer2189 <59424467+DragonSlayer2189@users.noreply.github.com>
2020-11-29 22:25:36 -0500

    Making TFM comply with itself

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_adventure.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_adventure.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.OP, source = SourceType.BOTH)
-@CommandParameters(description = "Quickly change your own gamemode to adventure, define someone's username to change theirs, or change everyone's gamemode on the server.", usage = "/<command> <[partialname] | -a>", aliases = "gma")
+@CommandParameters(description = "Quickly change your own gamemode to adventure, define someone's username to change theirs, or change everyone's gamemode on the server.", usage = "/<command> <player | -a>", aliases = "gma")
 public class Command_adventure extends FreedomCommand
 {
 
